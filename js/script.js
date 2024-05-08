0cad86a70b376fdb923224a28bf4843ea57a2484
Dohar Siregar <admin@dohar.com>
2024-05-08 11:09:46 +0700

    Membuat Interaksi Form dengan Javascript

--- js/script.js
+++ js/script.js
@@ -17,3 +17,42 @@ function showSlides(){
     dots[slideIndex-1].className += " active";
     setTimeout(showSlides, 2000);
 }
+
+function validationForm(){
+    const nama = document.forms['message-form']['name-input'].value
+    const email = document.forms['message-form']['email-input'].value
+    const choose = document.forms['message-form']['choose-input'].value
+
+    if (nama == ''){
+        document.getElementById("error-name").innerHTML = "Masukkan Nama !"
+        return false
+    } else {
+        setName(nama)
+    }
+    
+    if(email == ''){
+        document.getElementById("error-email").innerHTML = "Masukkan Email !"
+        return false
+    } else {
+        setName(email)
+    }
+    
+    if(choose == ''){
+        document.getElementById("error-choose").innerHTML = "Pilih Destinasi !"
+        return false
+    } else {
+        setName(choose)
+    }
+
+    alert('Data anda telah masuk !!!')
+}
+
+function setName(){
+    document.getElementById('error-name').innerHTML = ""
+    document.getElementById('error-email').innerHTML = ""
+    document.getElementById('error-choose').innerHTML = ""
+}
+
+document.getElementById('hubungi').addEventListener("click", function(){
+    alert('Terima Kasih Sudah Menghubungi Kami !!!')
+})
